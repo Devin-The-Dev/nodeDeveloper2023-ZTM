d2847c1e519a0dca57024a8f56eeb281ac85f728
Devin Udy <34194818+Devin-The-Dev@users.noreply.github.com>
2023-05-08 11:56:45 -0600

    Added a variable and a console.log to demonstrate module caching

--- customModule.js
+++ customModule.js
@@ -1,6 +1,13 @@
 const request = require('./fakeRequest');
 const response = require('./fakeResponse');
 
+// This variable is to demonstrate module caching
+// You'd think we'd get "Hello from fakeRequest.js" again, but we don't
+// Node has their modules cached (including customs). 
+// This is so automatic outputs (global console.logs) are not executed twice
+const { REQUEST_TIMEOUT } = require('./fakeRequest');
+/////////////////////////////////////////////////////////////////////////////
+
 function makeRequest(url, data) {
     request.send(url, data);
     return response.read();
